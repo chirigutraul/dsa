7c89971740dcb7cac05d9683bda506b65c2a62e2
chirigutraul <chirigutraul@gmail.com>
2022-12-03 16:38:30 +0200

    changed git account email, changed folder structure

---
+++ Common patterns/SlidingWindowPattern.js
@@ -0,0 +1,7 @@
+// write a function called maxSubarraySum
+// which accepts an array of integers and a number called n
+// the function should calculate the maximum sum of n consecutive elements in the array
+
+function maxSubarraySum(){
+  
+}
